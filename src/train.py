8185ec54561500a2dd4fe69c0cc88904a5029b6e
lab57 <luc.barrett57@gmail.com>
2025-06-09 21:11:41 -0400

    Add back 10,000 time limit

--- src/train.py
+++ src/train.py
@@ -39,8 +39,7 @@ def main():
     # Get the number of actions for our wrapper
     num_actions = base_env.action_space.n
 
-    # env = gymnasium.wrappers.TimeLimit(base_env, max_episode_steps=2000)
-    env = base_env
+    env = gymnasium.wrappers.TimeLimit(base_env, max_episode_steps=10_000)
     env = Monitor(env, log_dir, allow_early_resets=True)
     
     # FlattenObservation must be applied LAST for the wrapper to work correctly
